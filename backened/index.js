bc12295467c3e1450bcb9306bb34ca45904e1797
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-05-18 22:37:02 +0530

    backened

--- backened/index.js
+++ backened/index.js
@@ -1,11 +1,30 @@
 
-const express=require("express")
-const app=express()
+const express = require("express")
+const cors=require("cors")
+const { connection } = require("./db/config")
+const { UserModel } = require("./Model/User.model")
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("app is working")
 })
 
-app.listen(4500,()=>{
-    console.log("server is runnig at port 4500")
+app.post("/register", async(req, res) => {
+    const userDetails=req.body
+    try{
+        const user=new UserModel(userDetails)
+        await user.save()
+        res.send(user)
+        
+    }catch(err){
+        res.send({"msg":" not registered","error":err.message})
+    }
+})
+
+
+
+app.listen(4500, () => {
+    console.log("server is running at port 4500")
 })
