9e90bd6fdce1052e4c7e25924d0dda1ce6a7be0b
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-05-19 14:06:10 +0530

    Login component created

--- frontened/src/Components/Login.jsx
+++ frontened/src/Components/Login.jsx
@@ -1,22 +1,52 @@
 
-import React from "react";
-import { useState } from "react";
-const Login = () => {
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-    const handleLogin=()=>{
-        console.log(email,password)
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate()
+    useEffect(() => {
+        const auth = localStorage.getItem("registeredUser")
+        if (auth) {
+            navigate("/")
+        }
+    })
+
+    const handleLogin = async () => {
+        console.log(email, password)
+        let result = await fetch("http://localhost:4500/login", {
+            method: "post",
+            body: JSON.stringify({ email, password }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+
+        });
+        result = await result.json();
+        console.log(result)
+        if (result.name) {
+            localStorage.setItem("registeredUser", JSON.stringify(result))
+            navigate("/")
+        } else {
+            alert("please enter correct details")
+        }
+
     }
     return (
-        <>
-        <h1>Login Page</h1>
+        <div className="login">
+            <h1>Login Page</h1>
             <input className="inputField" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email" />
 
             <input className="inputField" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" />
 
             <button onClick={handleLogin} type="button" className="signUpButton">Login</button>
-        </>
+        </div>
     )
 }
+
+
+
 export default Login;
