4260e1ec7f25f23138f7e7b039f91bc3038ab0a9
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-05-21 08:19:00 +0530

    backened setup done

--- backened/index.js
+++ backened/index.js
@@ -1,45 +1,12 @@
 
-const express = require("express")
-const cors=require("cors")
-const { connection } = require("./db/config")
-const { UserModel } = require("./Model/User.model")
-const app = express()
+const express=require("express")
+const app=express()
 app.use(express.json())
-app.use(cors())
 
-app.get("/", (req, res) => {
+app.get("/",(req,res)=>{
     res.send("app is working")
 })
 
-app.post("/register", async(req, res) => {
-    const userDetails=req.body
-    try{
-        const user=new UserModel(userDetails)
-        await user.save()
-        res.send(user)
-        
-    }catch(err){
-        res.send({"msg":" not registered","error":err.message})
-    }
-})
-
-app.post("/login",async (req,res)=>{
-  const {email,password}=(req.body)
-  try{
-    const user=await UserModel.find({email,password})
-    if(user.length>0){
-        res.send(user)
-    }else{
-        res.send({"msg":" wrong credential"})
-        
-    }
-  }catch(err){
-    res.send({"msg":"something went wrong","err":err.message})
-        
-  }
-})
-
-
-app.listen(4500, () => {
+app.listen((4500,()=>{
     console.log("server is running at port 4500")
-})
+}))
