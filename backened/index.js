20b169891eb3a273a72c2ac7750a243d4bc3e148
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-05-21 10:54:20 +0530

    signup api connected tro database

--- backened/index.js
+++ backened/index.js
@@ -1,12 +1,21 @@
 
-const express=require("express")
-const app=express()
+const express = require("express")
+const cors=require("cors")
+const { connection } = require("./db/database")
+const { UserModel } = require("./db/users")
+const app = express()
 app.use(express.json())
+app.use(cors())
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("app is working")
 })
+app.post("/register", async (req, res) => {
+    let user = new UserModel(req.body)
+    let result = await user.save()
+    res.send(result)
+})
 
-app.listen((4500,()=>{
-    console.log("server is running at port 4500")
-}))
+app.listen(5500, () => {
+    console.log("server is running at port 5500")
+})
