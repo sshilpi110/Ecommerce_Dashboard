a387d7167309383d46473489dbc0ed78b694542c
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-05-21 21:19:04 +0530

    products listing get api created and working

--- backened/index.js
+++ backened/index.js
@@ -40,6 +40,15 @@ app.post("/add-product",async(req,res)=>{
     res.send(result)
 })
 
+app.get("/products",async(req,res)=>{
+    let products= await ProductModel.find()
+    if(products.length>0){
+        res.send(products)
+    }else{
+        res.send({"msg":"product not found"})
+    }
+})
+
 
 app.listen(5500, () => {
     console.log("server is running at port 5500")
